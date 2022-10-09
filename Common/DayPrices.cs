8e99716be64989403b874419bb05e21e1e6a4322
domaskal <domaskal@gmail.com>
2022-10-09 15:03:48 +0300

    [ADD] Common lib - DayPrices

---
+++ Common/DayPrices.cs
@@ -0,0 +1,40 @@
+﻿namespace Common
+{
+    public struct DayPrices : IComparable<DayPrices>
+    {
+        public DateTime Date { get; }
+        public Double[] HourlyPrices { get; }
+
+        public DayPrices(DateTime date, Double[] prices)
+        {
+            if (prices == null || prices.Length != 24)
+            {
+                throw new ArgumentException("Prices should have be non-null and have a length of 24!");
+            }
+
+            Date = date.Date;
+            HourlyPrices = prices;
+        }
+
+        /* Compares DayPrice dates */
+        public int CompareTo(DayPrices other)
+        {
+            if (this.Date < other.Date)
+            {
+                return 1;
+            }
+            else if (this.Date > other.Date)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        /* Returns true if the prices between two objects are the same */
+        public bool HasSamePrices(DayPrices other)
+        {
+            return this.HourlyPrices.SequenceEqual(other.HourlyPrices);
+        }
+    }
+}
