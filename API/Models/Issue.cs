e833a242432d0982b89469f302d45ae87f7704b1
Robertas <robertasburacevskis@gmail.com>
2022-10-10 21:51:30 +0300

    Endpoints Creation
    Created Get/Post/Put/Delete endpoints with some working sample data.

---
+++ API/Models/Issue.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required] //This property must have a value
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public IssueType IssueType { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Completed { get; set; }
+
+    }
+
+    public enum Priority
+    {
+        Low, Medium, High
+    }
+
+    public enum IssueType
+    {
+        Feature, Bug, Documentation
+    }
+}
