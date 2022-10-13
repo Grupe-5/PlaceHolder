a87f46631dbd991de84b122cb04e77eb943fac08
domaskal <domaskal@gmail.com>
2022-10-13 19:29:03 +0300

    [ADD] AsyncLazy PriceBrowser

---
+++ ScraperLib/PriceBrowser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+using PuppeteerSharp;
+
+namespace ScraperLib
+{
+    /* TODO: Implement OnProcessExit */
+    sealed internal class PriceBrowser
+    {
+        private PriceBrowser() { }
+
+        /* TODO: How to use dependency injection here? */
+        private static readonly AsyncLazy<IBrowser> _browser = new AsyncLazy<IBrowser> (async () =>
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+            return await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+        });
+
+        public static async Task<IPage> CreatePageAsync(String url, String waitSelector)
+        {
+            var browser = await _browser;
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync(url);
+            await page.WaitForSelectorAsync(waitSelector);
+            return page;
+        }
+    }
+}
