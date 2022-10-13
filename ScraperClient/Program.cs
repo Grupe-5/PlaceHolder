53449346e3bcb1313601d5366598999cffe24201
domaskal <domaskal@gmail.com>
2022-10-13 19:34:38 +0300

    [UPD] ScraperClient to use new IFetcher interface

--- ScraperClient/Program.cs
+++ ScraperClient/Program.cs
@@ -4,31 +4,52 @@ using System.Reflection.Metadata;
 using System.Linq;
 using ScraperLib;
 using Common;
+using System.Globalization;
 
 namespace ScraperClient
 {
+    public static class StringExtension
+    {
+        public static DateTime? IsValidDate(this string str, string format)
+        {
+            if (!DateTime.TryParseExact(str, format, new CultureInfo("en-US"), DateTimeStyles.None, out DateTime date))
+            {
+                return null;
+            }
+
+            return date;
+        }
+    }
+
     internal class Program
     {
+        static DateTime ReadDate(string message)
+        {
+            DateTime? startDate = null;
+            while (!startDate.HasValue)
+            {
+                Console.WriteLine(message);
+                var text = Console.ReadLine();
+                if (text != null)
+                {
+                    startDate = text.IsValidDate("yyyy-MM-dd");
+                }
+            }
+
+            return startDate.Value;
+        }
+
         static async Task Main(string[] _)
         {
             IFetcher fetcher = new PriceFetcher();
-            await fetcher.InitAsync();
-
-            Console.WriteLine("Enter start date (yyyy-MM-dd): ");
-            var start = Console.ReadLine();
-            var startDate = DateTime.Parse(start);
-
-            Console.WriteLine("Enter end date (yyyy-MM-dd): ");
-            var end = Console.ReadLine();
-            var endDate = DateTime.Parse(end);
+            var startDate = ReadDate("Enter start date (yyyy-MM-dd): ");
+            var endDate = ReadDate("Enter end date (yyyy-MM-dd): ");
 
             var prices = await fetcher.GetDayPricesAsync(startDate, endDate);
             foreach(var price in prices)
             {
                 Console.WriteLine($"{price.Date:yyyy-MM-dd} {string.Join(", ", price.HourlyPrices)}");
             }
-
-            await fetcher.DeinitAsync();
         }
     }
 }
