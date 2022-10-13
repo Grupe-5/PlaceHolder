f6d1923be270f9f431487f2582963ed349b85486
domaskal <domaskal@gmail.com>
2022-10-13 20:19:09 +0300

    [ADD] ModelBuilder for PriceDbContext

--- API/Data/PriceDbContext.cs
+++ API/Data/PriceDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Common;
+using PuppeteerSharp;
+using System.Text.Json;
 
 namespace API.Data
 {
@@ -10,5 +12,17 @@ namespace API.Data
         }
 
         public DbSet<DayPrices> DayPrices => Set<DayPrices>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<DayPrices>()
+                .HasKey(e => new {e.DaysSinceUnixEpoch});
+            modelBuilder.Entity<DayPrices>()
+            .Property(e => e.HourlyPrices)
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                v => JsonSerializer.Deserialize<Double[]>(v, (JsonSerializerOptions)null));
+            }
     }
 }
