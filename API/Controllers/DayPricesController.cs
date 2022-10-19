ba3fb333a9fe02c8bfbb8d9f58702b41ad869f32
domaskal <domaskal@gmail.com>
2022-10-19 12:29:30 +0300

    [ADD] DayPrices Controller and DbContext

---
+++ API/Controllers/DayPricesController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace trackingapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DayPricesController : ControllerBase
+    {
+        private readonly DayPricesDbContext _context;
+        public DayPricesController(DayPricesDbContext context) { _context = context; }
+
+        [HttpGet("{date}")]
+        [ProducesResponseType(typeof(DayPrices), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetById(String date)
+        {
+            if (!DateTime.TryParse(date, out DateTime dateVal))
+            {
+                return BadRequest("Date must be supplied in this format: yyyy-MM-dd");
+            }
+
+            var key = dateVal.Date.DaysSinceUnixEpoch();
+            var prices = await _context.DayPrices.FindAsync(key);
+            if (prices != null)
+            {
+                await _context.DayPrices.AddAsync(prices);
+            }
+
+            return prices == null ? NotFound() : Ok(prices);
+        }
+    }
+}
---
+++ API/Data/DayPricesDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Common;
+using PuppeteerSharp;
+using System.Text.Json;
+
+namespace API.Data
+{
+    public class DayPricesDbContext : DbContext
+    {
+        public DayPricesDbContext(DbContextOptions<DayPricesDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DayPrices> DayPrices => Set<DayPrices>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<DayPrices>()
+                .HasKey(e => new { e.DaysSinceUnixEpoch });
+            modelBuilder.Entity<DayPrices>()
+                .Property(e => e.HourlyPrices)
+                .HasConversion(
+                    v => JsonSerializer.Serialize(v, (JsonSerializerOptions?)null),
+                    v => JsonSerializer.Deserialize<double[]>(v, (JsonSerializerOptions?)null));
+        }
+    }
+}
