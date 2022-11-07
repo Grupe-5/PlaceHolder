580f79f63c21bc27dfe3cd30ea542c698899beb7
Nedux <nedasgulb@gmail.com>
2022-11-07 20:29:32 +0200

    settings page + fixed old PR issues

---
+++ GP3.Client/Services/SettingsService.cs
@@ -0,0 +1,25 @@
+﻿namespace GP3.Client.Services
+{
+    public class SettingsService
+    {
+        UserSettings userSettings;
+        public async Task<UserSettings> GetSettings1()
+        {            
+            userSettings = new UserSettings();
+            userSettings.priceChangeNotf = true;
+            userSettings.lowestPriceNotf = true;
+            userSettings.locations = new string[1];
+            userSettings.locations[0] = "Lithuania";
+            userSettings.userLocation = "Lithuania";
+
+            // GET API
+            return userSettings;
+        }
+
+        public async Task<bool> PutSettings(UserSettings userSettings)
+        {
+            // PUT API   
+            return true;
+        }
+    }
+}
