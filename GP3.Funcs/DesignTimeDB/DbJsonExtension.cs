456dbf083a282064a209be37a5413e7c6c92a624
domaskal <domaskal@gmail.com>
2022-11-07 22:52:04 +0200

    [ADD] Design-time DB config

---
+++ GP3.Funcs/DesignTimeDB/DbJsonExtension.cs
@@ -0,0 +1,21 @@
+﻿using GP3.Common.Constants;
+using GP3.Funcs.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GP3.Funcs.DesignTimeDB
+{
+    public static class DbJsonExtension<TContext> where TContext : DbContext
+    {
+        public static DbContextOptions<TContext> GetOptions()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+            optionsBuilder.UseMySqlMig<TContext>(config.GetConnectionString(ConnStrings.SQL), ConnStrings.ContextAssembly);
+            return optionsBuilder.Options;
+        }
+    }
+}
---
+++ GP3.Funcs/DesignTimeDB/IntegrationContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using GP3.Common.DB;
+using Microsoft.EntityFrameworkCore.Design;
+
+using Context = GP3.Common.DB.IntegrationDbContext;
+namespace GP3.Funcs.DesignTimeDB
+{
+    public class IntegrationContextFactory : IDesignTimeDbContextFactory<Context>
+    {
+        public Context CreateDbContext(string[] args)
+        {
+            return new Context(DbJsonExtension<Context>.GetOptions());
+        }
+    }
+}
