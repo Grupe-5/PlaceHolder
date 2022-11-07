5bdac5c64cac4ba98e87cea5db655edb2784e559
domaskal <domaskal@gmail.com>
2022-11-07 22:47:15 +0200

    [UPD] Price HTTP function

--- GP3.Funcs/Functions/HTTP/PriceHttp.cs
+++ GP3.Funcs/Functions/HTTP/PriceHttp.cs
@@ -1,6 +1,7 @@
 using GP3.Common.Constants;
 using GP3.Common.Entities;
 using GP3.Common.Repositories;
+using GP3.Funcs.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Extensions.JwtCustomHandler;
@@ -8,71 +9,47 @@ using Microsoft.Azure.Functions.Extensions.JwtCustomHandler.Interface;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Net;
 using System.Threading.Tasks;
 
-namespace GP3.Funcs
+namespace GP3.Funcs.Functions.HTTP
 {
-    public class PriceFunc
+    public class PriceHttp
     {
-        private readonly ILogger<PriceFunc> _logger;
         private readonly IDayPriceRepository _priceRepository;
-        /*
-         * TODO: Firebase token provider could be replaced with generic token provider 
-         * See: gh - kuromukira/azure-functions-jwt-validation-extension
-         */
-        private readonly IFirebaseTokenProvider _tokenProvider;
+        private readonly ReqAuthService _reqAuthService;
 
-        public PriceFunc(IDayPriceRepository priceRepository, IFirebaseTokenProvider tokenProvider, ILogger<PriceFunc> log)
+        public PriceHttp(IDayPriceRepository priceRepository, ReqAuthService reqAuthService)
         {
             _priceRepository = priceRepository;
-            _logger = log;
-            _tokenProvider = tokenProvider;
+            _reqAuthService = reqAuthService;
         }
 
-        [FunctionName("PriceHttpGet")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "date" })]
+        [FunctionName("PriceHttp")]
+        [OpenApiOperation(operationId: "Get Price")]
+        [OpenApiSecurity("bearer_auth", SecuritySchemeType.Http, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
         [OpenApiParameter(name: "date", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "Date in yyyy-MM-dd format")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(DayPrice), Description = "Price found")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.NotFound, contentType: "text/plain", bodyType: typeof(string), Description = "Price not found")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/plain", bodyType: typeof(string), Description = "Invalid date")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.Unauthorized, contentType: "text/plain", bodyType: typeof(string), Description = "Invalid token")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Routes.GetPrice)] HttpRequest req)
-        {
-            try
+        public async Task<IActionResult> GetPrice([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Routes.Price)] HttpRequest req)
+            => await _reqAuthService.HandledAuthAction(req, async (req, _) =>
             {
-                if (req.Headers is null)
-                    throw new NullReferenceException();
-                else if (req.Body is null)
-                    throw new NullReferenceException();
-
-                var tokenResult = await _tokenProvider.ValidateToken(req);
-                if (tokenResult.Status != AccessTokenStatus.Valid)
-                    return new UnauthorizedObjectResult("Invalid access token");
-
                 string date = req.Query["date"];
                 if (!DateTime.TryParse(date, out DateTime dateVal))
-                {
                     return new BadRequestObjectResult("Date must be supplied in this format: yyyy-MM-dd");
-                }
 
-                var price = await _priceRepository.GetByDateAsync(dateVal);
+                var price = await _priceRepository.GetDayPriceAsync(dateVal);
                 if (price == null)
-                {
                     return new NotFoundObjectResult($"Price for {dateVal:yyyy-MM-dd} not available!");
-                }
-                return new OkObjectResult(price);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError("Server encountered an error: {0}", e.Message);
-                return new BadRequestObjectResult("Web server encountered an error");
-            }
-        }
+                else
+                    return new OkObjectResult(price);
+            });
     }
 }
 
