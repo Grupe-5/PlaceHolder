55fc987acfa74cddbb868edf6160880db92bd721
domaskal <domaskal@gmail.com>
2022-11-07 22:54:17 +0200

    [ADD] TaskForEach extension

---
+++ GP3.Common/Extensions/TaskForEachExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP3.Common.Extensions
+{
+    public static class TaskForEachExtension
+    {
+        public static Task ForEachAsync<T>(this IEnumerable<T> sequence, Func<T, Task> action)
+        {
+            return Task.WhenAll(sequence.Select(action));
+        }
+    }
+}
