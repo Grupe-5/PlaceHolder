a21b864ec2d2d01d7a16d8eb9f08462681f16990
domaskal <domaskal@gmail.com>
2022-11-07 22:53:07 +0200

    [UPD] DbContexts
    * Fix warning for DayPrice DB Context
    * Add IntegrationCallback DB Context

--- GP3.Common/DB/DayPriceDbContext.cs
+++ GP3.Common/DB/DayPriceDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using GP3.Common.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Text.Json;
 
 namespace GP3.Common.DB
@@ -15,14 +17,24 @@ namespace GP3.Common.DB
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder
-                .Entity<DayPrice>()
+            modelBuilder.Entity<DayPrice>()
                 .HasKey(e => new { e.DaysSinceUnixEpoch });
+
             modelBuilder.Entity<DayPrice>()
                 .Property(e => e.HourlyPrices)
                 .HasConversion(
                     v => JsonSerializer.Serialize(v, (JsonSerializerOptions?)null),
                     v => JsonSerializer.Deserialize<double[]>(v, (JsonSerializerOptions?)null) ?? new double[0]);
+
+            var priceComparer = new ValueComparer<double[]>(
+                (c1, c2) => (c1 != null && c2 != null) && c1.SequenceEqual(c2),
+                c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                c => c.ToArray());
+
+            modelBuilder.Entity<DayPrice>()
+                .Property(e => e.HourlyPrices)
+                .Metadata
+                .SetValueComparer(priceComparer);
         }
     }
 }
---
+++ GP3.Common/DB/IntegrationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GP3.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GP3.Common.DB
+{
+    public class IntegrationDbContext : DbContext
+    {
+        public IntegrationDbContext(DbContextOptions<IntegrationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<IntegrationCallback> Integrations => Set<IntegrationCallback>();
+    }
+}
