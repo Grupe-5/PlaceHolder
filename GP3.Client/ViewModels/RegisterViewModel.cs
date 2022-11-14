e9c5637408773cf86b75607ab005c385eb4eeccd
Afterburn7 <legotronas@gmail.com>
2022-11-14 23:25:07 +0200

    Fixed regex for passwords when registering
    Default firebase validation is weird

--- GP3.Client/ViewModels/RegisterViewModel.cs
+++ GP3.Client/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using GP3.Client.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Text.RegularExpressions;
 
 namespace GP3.Client.ViewModels
 {
@@ -8,26 +9,59 @@ namespace GP3.Client.ViewModels
     {
         readonly AuthService authService;
 
-        public RegisterViewModel(AuthService authService)
-        {
-            this.authService = authService;
-        }
-
         [ObservableProperty]
         string email;
 
         [ObservableProperty]
         string password;
 
+        [ObservableProperty]
+        string repeatedPassword;
+
+        public RegisterViewModel(AuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        bool IsValidPasswordRegex(string password)
+        {
+            bool isPassword = Regex.IsMatch(
+                password.Replace("\"", @"\"),
+                @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[\\#£{};':`,./|?!@$%^&*-]).{8,}$");
+            return isPassword;
+        }
+
+
         [RelayCommand]
         public async Task RegisterAsync()
         {
             if (IsBusy)
                 return;
+
+            if(password != repeatedPassword)
+            {
+                await Shell.Current.DisplayAlert("Error",
+                    "Passwords do not match",
+                    "OK");
+               
+                return;
+            }
+
+            if (IsValidPasswordRegex(password) == false)
+            {
+                await Shell.Current.DisplayAlert("Error", 
+                    "Password must contain at least 1 upppercase, " +
+                    "lowercase characters and special symbols!\n" +
+                    "Password must be at least 8 characters long!",
+                    "OK");
+               
+                return;
+            }
+
             try
             {
                 IsBusy = true;
-                await authService.RegisterAsync(email, password);
+                await authService.RegisterAsync(email, password.Replace("\"", @"\"));
                 await Shell.Current.GoToAsync(nameof(HomePage));
             }
             catch (Exception ex)
