8fb0b20cd572d1233594fc35794f1ae6db52068f
domaskal <domaskal@gmail.com>
2022-11-14 18:19:56 +0200

    [ADD] First test to test coverage

---
+++ GP3.Tests/Common/Entities/DayPriceTest.cs
@@ -0,0 +1,32 @@
+﻿using GP3.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP3.Tests.Common.Entities
+{
+    public class DayPriceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void FromUnixEpochTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DaysSinceUnixEpochTest()
+        {
+            var start = DateTime.UnixEpoch;
+            Assert.That(start.DaysSinceUnixEpoch(), Is.EqualTo(0));
+            Assert.Pass();
+        }
+    }
+}
