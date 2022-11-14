60685f2395818370dd2c91a94f9e89f9abd76d1b
domaskal <domaskal@gmail.com>
2022-11-14 22:09:14 +0200

    [UPD] Assume passed time as UTC & convert to CEST

--- GP3.Funcs/Functions/HTTP/PriceHttp.cs
+++ GP3.Funcs/Functions/HTTP/PriceHttp.cs
@@ -2,6 +2,7 @@ using GP3.Common.Constants;
 using GP3.Common.Entities;
 using GP3.Common.Repositories;
 using GP3.Funcs.Services;
+using GP3.Scraper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -50,7 +51,7 @@ namespace GP3.Funcs.Functions.HTTP
         [FunctionName("PriceOffsetHttp")]
         [OpenApiOperation(operationId: "Get Price /w offset")]
         [OpenApiSecurity("bearer_auth", SecuritySchemeType.Http, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
-        [OpenApiParameter(name: "date", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "Date and hour in yyyy-MM-dd:HH format")]
+        [OpenApiParameter(name: "date", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "Date and hour in yyyy-MM-dd:HH format (assumed as UTC!)")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(double[]), Description = "Prices that were available")]
         public async Task<IActionResult> GetPriceOffset([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Routes.PriceOffset)] HttpRequest req)
             => await _reqAuthService.HandledAuthAction(req, async (req, _) =>
@@ -59,6 +60,7 @@ namespace GP3.Funcs.Functions.HTTP
                 if (!DateTime.TryParseExact(date, "yyyy-MM-dd:HH", null, System.Globalization.DateTimeStyles.None, out DateTime dateVal))
                     return new BadRequestObjectResult("Date must be supplied in this format: yyyy-MM-dd:HH");
 
+                dateVal = dateVal.UTCtoCEST();
                 List<double> priceCollection = new();
 
                 var priceDay = await _priceRepository.GetDayPriceAsync(dateVal);
@@ -76,7 +78,6 @@ namespace GP3.Funcs.Functions.HTTP
 
                 return new OkObjectResult(priceCollection);
             });
-
     }
 }
 
