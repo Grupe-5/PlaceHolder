860b8dbdd4a0aa404c55b2c984c1d26e1775ef94
domaskal <domaskal@gmail.com>
2022-11-14 22:04:26 +0200

    [ADD] Price /w Offset HTTP GET function

--- GP3.Funcs/Functions/HTTP/PriceHttp.cs
+++ GP3.Funcs/Functions/HTTP/PriceHttp.cs
@@ -10,6 +10,8 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -44,6 +46,37 @@ namespace GP3.Funcs.Functions.HTTP
                 else
                     return new OkObjectResult(price);
             });
+
+        [FunctionName("PriceOffsetHttp")]
+        [OpenApiOperation(operationId: "Get Price /w offset")]
+        [OpenApiSecurity("bearer_auth", SecuritySchemeType.Http, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+        [OpenApiParameter(name: "date", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "Date and hour in yyyy-MM-dd:HH format")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(double[]), Description = "Prices that were available")]
+        public async Task<IActionResult> GetPriceOffset([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = Routes.PriceOffset)] HttpRequest req)
+            => await _reqAuthService.HandledAuthAction(req, async (req, _) =>
+            {
+                string date = req.Query["date"];
+                if (!DateTime.TryParseExact(date, "yyyy-MM-dd:HH", null, System.Globalization.DateTimeStyles.None, out DateTime dateVal))
+                    return new BadRequestObjectResult("Date must be supplied in this format: yyyy-MM-dd:HH");
+
+                List<double> priceCollection = new();
+
+                var priceDay = await _priceRepository.GetDayPriceAsync(dateVal);
+                if (priceDay == null)
+                {
+                    return new OkObjectResult(priceCollection);
+                }
+
+                priceCollection.AddRange(priceDay.HourlyPrices.Skip(dateVal.Hour));
+                var priceNext = await _priceRepository.GetDayPriceAsync(dateVal.AddDays(1));
+                if (priceNext != null)
+                {
+                    priceCollection.AddRange(priceNext.HourlyPrices.Take(dateVal.Hour));
+                }
+
+                return new OkObjectResult(priceCollection);
+            });
+
     }
 }
 
