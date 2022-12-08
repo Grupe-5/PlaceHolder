1e94a1891c30a6c03cbfa778817c0876e7a1bc1c
Afterburn7 <legotronas@gmail.com>
2022-12-08 16:50:16 +0200

    Sentry.IO implementation
    Sentry.io tracks errors and app performance on the client side.
    
    Download Sentry.MAUI NuGet to Client proj.

--- GP3.Client/MauiProgram.cs
+++ GP3.Client/MauiProgram.cs
@@ -1,10 +1,11 @@
-﻿using DevExpress.Maui;
+﻿using Android.OS;
+using DevExpress.Maui;
 using GP3.Client.Refit;
 using GP3.Client.Services;
 using GP3.Client.ViewModels;
 using MonkeyCache;
 using MonkeyCache.FileStore;
-
+using Sentry;
 
 namespace GP3.Client;
 
@@ -16,6 +17,12 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseSentry(options =>
+            {
+                // The DSN is the only required setting.
+                options.Dsn = "https://369041dcedd24381ad160911a69b287c@o4504267915264000.ingest.sentry.io/4504293859852288";
+
+            })
             .UseDevExpress()
             .ConfigureFonts(fonts =>
             {
