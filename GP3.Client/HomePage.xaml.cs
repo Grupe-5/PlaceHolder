5ca00d0db6a016a3544ec2177e5d0e6bef317755
Nedux <nedasgulb@gmail.com>
2022-12-15 14:38:29 +0200

    Made homePage more beutifull

--- GP3.Client/HomePage.xaml.cs
+++ GP3.Client/HomePage.xaml.cs
@@ -8,4 +8,8 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
         BindingContext = vm;
     }
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        ((HomeViewModel)BindingContext).SetLowPriceLine();
+    }
 }
