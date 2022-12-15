0ec38502dd285ef4915fa53c97cdf929c0de15c1
Robertas <robertasburacevskis@gmail.com>
2022-12-15 12:16:01 +0200

    Alternative DateChanger
    Made new simpler approach for DateChanger for graph in MainPage.

--- GP3.Client/ViewModels/HomeViewModel.cs
+++ GP3.Client/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DevExpress.Maui.Core.Internal;
 using GP3.Client.Models;
 using GP3.Client.Refit;
@@ -12,13 +13,20 @@ namespace GP3.Client.ViewModels
         private readonly IPriceApi _priceApi;
         public ObservableCollection<HourPriceFormated> HourPricesFormated { get; } = new();
 
+        private DateTime today;
+
         public HomeViewModel(IPriceApi priceApi, SettingsService settingsService)
         {
             _priceApi = priceApi;
             Title = "Home";
-            GetPricesAsync();
+
+            today = DateTime.Now.Date;
+            selectedDate = today;
+            selectedDateFormated = selectedDate.ToShortDateString();
 
             currHour = DateTime.Now.Hour;
+            GetPricesAsync();
+
             currUserLowPriceDefinition = settingsService.Settings.lowPriceMark;
         }
 
@@ -27,12 +35,42 @@ namespace GP3.Client.ViewModels
 
         [ObservableProperty]
         public int currHour;
+
+        [ObservableProperty]
+        public string selectedDateFormated;
+
+        [ObservableProperty]
+        public DateTime selectedDate;
+
+        [RelayCommand]
+        void DecreaseDate()
+        {
+            selectedDate = SelectedDate.AddDays(-1);
+            SelectedDateFormated = selectedDate.ToShortDateString();
+            GetPricesAsync();
+        }
+
+        [RelayCommand]
+        void IncreaseDate()
+        {
+            if (SelectedDate != today)
+            {
+                SelectedDate = SelectedDate.AddDays(1);
+                SelectedDateFormated = SelectedDate.ToShortDateString();
+                GetPricesAsync();
+            }
+            else
+            {
+                Shell.Current.DisplayAlert("Datos keisti negalima!", "Mes nezinom kokios kainos but rytoj", "OK");
+            }
+        }
+
         async public void GetPricesAsync()
         {
             try
             {
                 IsBusy = true;
-                var prices = await _priceApi.GetPriceOffsetAsync(DateTime.Today);
+                var prices = await _priceApi.GetPriceOffsetAsync(SelectedDate);
                 int index = 0;
                 HourPriceFormated hourPriceFormated;
                 HourPricesFormated.Clear();
